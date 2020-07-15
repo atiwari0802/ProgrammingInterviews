2784411e7ec4b80183a190eb14e1cf0940e3b1e2
Apurva Tiwari <tiwaria@Apurvas-MacBook-Pro-2.local>
2020-07-15 18:13:05 -0400

    Maximum Product Sub-Array

--- src/test/java/sorting/IntersectionOfArraysTest.java
+++ src/test/java/sorting/IntersectionOfArraysTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class IntersectionOfArraysTest {
@@ -24,5 +25,14 @@ public class IntersectionOfArraysTest {
 
         result.forEach(System.out::println);
 
+    }
+
+    @Test
+    public void test() {
+        List<Long> list = Arrays.asList(1594065485776L, 1593544794810L, 1593544178716L, 1593543888221L);
+
+        list.sort(Comparator.naturalOrder());
+
+        list.forEach(n -> System.out.printf(" %d", n));
     }
 }
